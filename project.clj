fbd0e940e5c840dfe71c46052a13c3cc210868ad
Peter Hudec <peterhudec@peterhudec.com>
2019-04-30 11:12:44 +0100

    Upgraded to Clojure 1.9.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cljsx "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[midje "1.9.8"]]}
              :midje {}})
 

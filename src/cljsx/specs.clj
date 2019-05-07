ae13337bae9e8a969548b9f8b6bf248b9a143501
Peter Hudec <peterhudec@peterhudec.com>
2019-05-07 14:04:53 +0100

    A sketch of props mergelist specs.
    Also contains the solution to proper props spec.

--- src/cljsx/specs.clj
+++ src/cljsx/specs.clj
@@ -24,7 +24,8 @@
 (s/def ::even-vector (s/coll-of ::form
                                 :kind even-vector?))
 
-(s/def ::spread-val (s/or :reference symbol?
+(s/def ::spread-val (s/or :reference #(and (symbol? %)
+                                           (not= % '>))
                           :map ::map
                           :jsx ::jsx-expression
                           :s-expression ::s-expression
@@ -85,13 +86,195 @@
                                      :tag-end ::props-tag-end
                                      :children ::forms))
 
+(s/conform ::props-jsx-expression '(<foo :a "A" ... xxx :b :c "C" > child))
+;; => {:tag-start [:intrinsic-tag <foo],
+;;     :props
+;;     [[:attr {:name :a, :value [:primitive "A"]}]
+;;      [:spread {:spread-operator ..., :spread-value [:reference xxx]}]
+;;      [:attr {:name :b}]
+;;      [:attr {:name :c, :value [:primitive "C"]}]],
+;;     :tag-end >,
+;;     :children [[:primitive child]]}
+
+(s/conform ::props '(:a "A" ... xxx :b :c "C"))
+;; => [[:attr {:name :a, :value [:primitive "A"]}]
+;;     [:spread {:spread-operator ..., :spread-value [:reference xxx]}]
+;;     [:attr {:name :b}]
+;;     [:attr {:name :c, :value [:primitive "C"]}]]
+
+(defn props->mergelist [props]
+  (let [props' (s/conform ::props props)]
+    (reduce (fn [a [prop-type prop]]
+              (conj a prop))
+            []
+            props')
+    #_(split-with #(not= (first %) :spread) props')))
+
+(props->mergelist '(:a "A" ... xxx :b :c "C"))
+;; => [[:attr {:name :a, :value [:primitive "A"]}]
+;;     [:spread {:spread-operator ..., :spread-value [:reference xxx]}]
+;;     [:attr {:name :b}]
+;;     [:attr {:name :c, :value [:primitive "C"]}]]
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Mergegroup attrs first
+
+(s/def ::prop-mergegroup (s/cat :attrs (s/* ::attr)
+                                :spread ::spread))
+
+(s/def ::prop-mergegroups (s/cat :mergegroups (s/* ::prop-mergegroup)
+                                 :trailing-attrs (s/? (s/* ::attr))))
+
+(s/conform ::prop-mergegroups '(:a :b "B" :c ... x :d :e ... y :f))
+
+
+(s/conform ::prop-mergegroups '(:a :b "B" :c ... x :d :e ... y))
+
+(s/conform ::prop-mergegroups '(... z
+                                    :a :b "B" :c
+                                    ... x
+                                    :d :e
+                                    ... y))
+
+(s/def ::props-jsx-expression-2 (s/cat :tag-start ::props-tag
+                                       :prop-mergegroups ::prop-mergegroups
+                                       :tag-end ::props-tag-end
+                                       :children ::forms))
+
+(s/conform ::props-jsx-expression-2
+           '(<foo :a :b "B" :c >))
+
+(s/conform ::props-jsx-expression-2
+           '(<foo ... zzz
+                  :a :b "B" :c
+                  ... 123
+                  :a :b
+                  ... yyy
+                  :a > "child"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Mergegroup spread first
+
+(s/def ::prop-mergegroup-b (s/cat 
+                            :spread ::spread
+                            :attrs (s/* ::attr)
+                            ))
+
+(s/def ::leading-attrs
+  (s/alt ;; :keyword keyword?
+   :keyword keyword?
+   :spread (s/cat :spread-operator ::spread-operator
+                  :spread-value ::spread-val))
+  ;; (s/? ::attr)
+  #_(s/* ::attr)
+  )
+
+
+;; (s/def ::prop-mergegroups-b (s/cat :leading-attrs (s/* ::attr)
+;;                                    :mergegroups (s/* ::prop-mergegroup-b)))
+(s/def ::prop-mergegroups-b (s/cat
+                             :head (s/alt
+                                    ;; :spread (s/cat :op ::spread-operator
+                                    ;;                :val ::spread-val-b)
+                                    :spread ::spread
+                                    :keyword keyword?
+                                    :attr (s/cat :name keyword?
+                                                 :value any?)
+                                    ;; :spread-operator ::spread-operator
+                                    )
+                             ;; :tail (s/* any?)
+                             :tail (s/* (s/alt
+                                         :spread ::spread
+                                         :keyword keyword?
+                                         :attr (s/cat :name keyword?
+                                                      :value (s/and any?
+                                                                    #(not= % '...)))
+                                         ;; :attrs (s/* ::attr)
+                                         ))
+
+                             ;; :leading-attrs ::leading-attrs
+                             ;; :mergegroups (s/* ::prop-mergegroup-b)
+                             ))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+(s/def ::prop-new (s/alt
+                   :spread ::spread
+                   :true-attr keyword?
+                   :val-attr (s/cat :name keyword?
+                                :value (s/and 
+                                              (complement keyword?)
+                                              #(not= % '...)
+                                              ::form))
+                   ))
+
+(s/def ::props-new (s/* ::prop-new))
+
+(s/def ::props-jsx-expression-new (s/cat :tag-start ::props-tag
+                                         :props ::props-new
+                                         :tag-end ::props-tag-end
+                                         :children ::forms))
+
+(s/conform ::props-jsx-expression-new
+           '(<foo >))
+
+(s/conform ::props-jsx-expression-new
+           '(<foo > child))
+
+(s/conform ::props-jsx-expression-new
+           '(<foo :a :b :c > child))
+
+(s/conform ::props-jsx-expression-new
+           '(<foo :a :b "B" :c
+                  ... xxx
+                  :d "D" :e :f "F"
+                  ... yyy > child child child))
+
+;; ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(s/conform ::prop-mergegroups-b '(... xxx))
+(s/conform ::prop-mergegroups-b '(... xxx :a :b "B" :c))
+(s/conform ::prop-mergegroups-b '(... xxx :a :b "B" :c ... yyy))
+(s/conform ::prop-mergegroups-b '(... xxx :a :b "B" :c ... yyy :d))
+(s/conform ::prop-mergegroups-b '(:a ... xxx))
+(s/conform ::prop-mergegroups-b '(:a "A" ... xxx))
+(s/conform ::prop-mergegroups-b '(:a "A" :aa ... xxx))
+
+(s/conform ::prop-mergegroups-b '(:a :b "B" :c ... x :d :e ... y :f))
+
+
+(s/conform ::prop-mergegroups-b '(:a :b "B" :c ... x :d :e ... y))
+
+(s/conform ::prop-mergegroups-b '(... z
+                                    :a :b "B" :c
+                                    ... x
+                                    :d :e
+                                    ... y))
+
+(s/def ::props-jsx-expression-2-b (s/cat :tag-start ::props-tag
+                                         :prop-mergegroups ::prop-mergegroups-b
+                                         :tag-end ::props-tag-end
+                                         :children ::forms))
+
+(s/conform ::props-jsx-expression-2-b
+           '(<foo :a :b "B" :c >))
+
+(s/conform ::props-jsx-expression-2-b
+           '(<foo ... zzz
+                  :a :b "B" :c
+                  ... 123
+                  :a :b
+                  ... yyy
+                  :a > "child"))
+
 (s/def ::jsx-expression
   ;; We need to check for seq?, otherwise vectors would match as
   ;; expressions e.g [<foo> bar] would be a valid JSX expression.
   (s/and seq?
          (s/or :simple-jsx-expression ::simple-jsx-expression
                :props-jsx-expression ::props-jsx-expression)))
-
+;; => :cljsx.specs/jsx-expression
 (defn- tag? [x]
   (and (symbol? x)
        (->> x
